a4a7c29f814299f4fc5e7c1a21be5aeddebfeba5
Zubeyir <zubabi@gmail.com>
2019-09-18 15:36:07 +0300

    First Commit

---
+++ ZYCalculator/ZYCalculator/Calculator.swift
@@ -0,0 +1,25 @@
+//
+//  Calculator.swift
+//  ZYCalculator
+//
+//  Created by Zübeyir Yayıkçı on 18.09.2019.
+//  Copyright © 2019 Zübeyir Yayıkçı. All rights reserved.
+//
+
+import Foundation
+
+public class Calculator{
+    private init(){}
+    
+    public static func Sum(num1: Int, num2: Int) ->Int
+    {
+        return num1 + num2
+    }
+    
+    public static func Sum(num1: Int, num2: Int) ->Int
+    {
+        return num1 + num2
+    }
+    
+    
+}
---
+++ ZYCalculator/ZYCalculator/ZYCalculator.h
@@ -0,0 +1,19 @@
+//
+//  ZYCalculator.h
+//  ZYCalculator
+//
+//  Created by Zübeyir Yayıkçı on 18.09.2019.
+//  Copyright © 2019 Zübeyir Yayıkçı. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZYCalculator.
+FOUNDATION_EXPORT double ZYCalculatorVersionNumber;
+
+//! Project version string for ZYCalculator.
+FOUNDATION_EXPORT const unsigned char ZYCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZYCalculator/PublicHeader.h>
+
+
